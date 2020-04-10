cc57910b437accbb3168ce850c73aa632bf6b680
AlexCsanyi <alex.csanyi@gmail.com>
2020-04-10 11:19:49 +0100

    started the node js version

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(adminRoute);
+app.use(shopRoute);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not Found</h1>");
+});
+
+app.listen(3000);
