355bdd96a829427ce0679f073e27fdf13b057512
AlexCsanyi <alex.csanyi@gmail.com>
2020-04-12 16:26:44 +0100

    removed html files, going forward with handlebars - added new routes

---
+++ controllers/shop.js
@@ -0,0 +1,44 @@
+const Product = require("../models/product");
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products.slice(0, 3),
+      pageTitle: "Shop | Featured",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "Products",
+      path: "/products",
+      hasProducts: products.length > 0,
+      activeProducts: true,
+      productCSS: true,
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+    productCSS: true,
+    activeCart: true,
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout Page",
+    path: "/checkout",
+    activeCheckout: true,
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-router.get("/add-product", productsController.getAddProduct);
+// /admin/ => GET
+router.get("/products", adminController.getAdminProducts);
+router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", productsController.postAddProduct);
+// /admin/ => POST
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
